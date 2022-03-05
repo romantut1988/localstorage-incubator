867c1e7f8aaf942659a531f2353e20b98d226af6
romantut1988 <romantut1988@mail.ru>
2022-03-05 11:17:22 +0300

    the main methods of interaction with localstorage.JSON stringify,parse.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
 
-  return (
-    <div className="App">
-      <h1>Localstorage</h1>
-    </div>
-  );
+    const [value, setValue] = useState<number>(0)
+
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+    const setLocalStorageHandler = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+        localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
+    }
+    const getFromLocalHandler = () => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
+    }
+    const clearLocalStorageHandler = () => {
+        localStorage.clear()
+        setValue(0)
+    }
+    const removeItemFromLocalStorageHandler = () => {
+        localStorage.removeItem('counterValue + 1')
+    }
+
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            <button onClick={setLocalStorageHandler}>setLocalStorage</button>
+            <button onClick={getFromLocalHandler}>getFromLocalStorage</button>
+            <button onClick={clearLocalStorageHandler}>clearLocalStorageHandler</button>
+            <button onClick={removeItemFromLocalStorageHandler}>removeItemFromLocalStorage</button>
+        </div>
+    );
 }
 
 export default App;
